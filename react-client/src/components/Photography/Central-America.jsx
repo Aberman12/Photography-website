a7d83e3e4ddd572bc0810dfdbb5f452ae808ce2f
Alex <aberman12@yahoo.com>
2018-11-04 15:55:46 -0800

    added ca pictures

--- react-client/src/components/Photography/Central-America.jsx
+++ react-client/src/components/Photography/Central-America.jsx
@@ -28,6 +28,70 @@ const CentralAmerica = props => (
           />
         </div>
       </div>
+      <div className="col-lg-4 col-md-6 col-sm-6">
+        <div className="thumbnail">
+          <img
+            src="https://scontent-lax3-2.xx.fbcdn.net/v/t1.0-9/45379053_10156113153634072_2356790035832897536_o.jpg?_nc_cat=103&_nc_ht=scontent-lax3-2.xx&oh=a516d16797d13d48afff25650e6a2786&oe=5C41AB3B"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="col-lg-4 col-md-6 col-sm-6">
+        <div className="thumbnail">
+          <img
+            src="https://scontent-lax3-2.xx.fbcdn.net/v/t1.0-9/45389597_10156113158569072_4965410187667046400_o.jpg?_nc_cat=110&_nc_ht=scontent-lax3-2.xx&oh=35541b52e6b4077a9a27f3dd791521ea&oe=5C4381B0"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="col-lg-4 col-md-6 col-sm-6">
+        <div className="thumbnail">
+          <img
+            src="https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/45430408_10156113158684072_1909017975575281664_o.jpg?_nc_cat=105&_nc_ht=scontent-lax3-1.xx&oh=b2184c3eb256506f6b1669767ac6f9dc&oe=5C84AC95"
+            alt=""
+          />
+        </div>
+      </div>{" "}
+      <div className="col-lg-4 col-md-6 col-sm-6">
+        <div className="thumbnail">
+          <img
+            src="https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/45434333_10156113158754072_5519407054126579712_o.jpg?_nc_cat=105&_nc_ht=scontent-lax3-1.xx&oh=76b4276995497af76c5d163164b728e2&oe=5C42E63A"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="col-lg-4 col-md-6 col-sm-6">
+        <div className="thumbnail">
+          <img
+            src="https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/45447417_10156113163439072_4518959250759745536_o.jpg?_nc_cat=101&_nc_ht=scontent-lax3-1.xx&oh=a05e64a639e72b93841a8a531afd41da&oe=5C782C03"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="col-lg-4 col-md-6 col-sm-6">
+        <div className="thumbnail">
+          <img
+            src="https://scontent-lax3-2.xx.fbcdn.net/v/t1.0-9/45459386_10156113163259072_7502061402178191360_o.jpg?_nc_cat=109&_nc_ht=scontent-lax3-2.xx&oh=e3800ddf6474a1ba3c15408300f04ff6&oe=5C48E046"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="col-lg-4 col-md-6 col-sm-6">
+        <div className="thumbnail">
+          <img
+            src="https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/45498044_10156113163279072_6671401236639514624_o.jpg?_nc_cat=100&_nc_ht=scontent-lax3-1.xx&oh=26e2d7f89c99d712e0fe1e9b1835922e&oe=5C46D3B4"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="col-lg-4 col-md-6 col-sm-6">
+        <div className="thumbnail">
+          <img
+            src="https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/45424030_10156113163474072_464897155723689984_o.jpg?_nc_cat=110&_nc_ht=scontent-lax3-1.xx&oh=c4c3283b587299799be6acc554617018&oe=5C7765CE"
+            alt=""
+          />
+        </div>
+      </div>
       {/* <div className="col-lg-4 col-md-6 col-sm-6">
         <div className="thumbnail">
           <img src="" alt="" />
